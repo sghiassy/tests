ba45d982e6f295a1effaf1a63aa0abe6cbaf544a
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-06-22 13:16:42 -0700

    Broken' git push

--- 2015/12-ReactNative-Playground/index.ios.js
+++ 2015/12-ReactNative-Playground/index.ios.js
@@ -1,53 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
 var React = require('react-native');
+var Overlay = require('react-native-overlay');
+var BlurView = require('react-native-blur').BlurView;
+
 var {
   AppRegistry,
-  StyleSheet,
-  Text,
   View,
+  ActivityIndicatorIOS,
+  StyleSheet,
+  Image,
 } = React;
 
-var AwesomeProject = React.createClass({
-  render: function() {
+var LoadingOverlay = React.createClass({
+  getDefaultProps(): StateObject {
+    return {
+      isVisible: false
+    }
+  },
+
+  render(): ReactElement {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Overlay isVisible={this.props.isVisible}>
+        <BlurView style={styles.background} blurType="dark">
+          <ActivityIndicatorIOS
+            size="large"
+            animating={true}
+            style={styles.spinner} />
+        </BlurView>
+      </Overlay>
     );
   }
 });
 
 var styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+})
+
+var AwesomeProject = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Image source={require('image!announcement')} style={styles.image} />
+
+        { /* It doesn't matter where we put this component, it can be nested */ }
+        { /* anywhere within your component tree */ }
+        <LoadingOverlay isVisible={true} />
+      </View>
+    );
+  }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
