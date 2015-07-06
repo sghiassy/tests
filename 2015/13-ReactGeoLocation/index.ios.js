9c4c38a1f1f526858ce057894077460bbec78d95
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-07-06 16:19:14 -0700

    GeoLocation works

--- 2015/13-ReactGeoLocation/index.ios.js
+++ 2015/13-ReactGeoLocation/index.ios.js
@@ -1,31 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+var { AppRegistry, StyleSheet, Text, View } = React;
+var GeoLocation = require('./GeoLocation');
 
 var ReactGeoLocation = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <GeoLocation />
       </View>
     );
   }
