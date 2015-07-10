ba8f934e48fe7632f01b18daa1c829441e289ea0
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-07-10 15:38:52 -0700

    Test is working

--- 2015/14-React/TestyMcTest/src/Header.js
+++ 2015/14-React/TestyMcTest/src/Header.js
@@ -6,8 +6,23 @@ var { AppRegistry, StyleSheet, Text, View } = React;
 class Header extends React.Component {
   render() {
     return(
-      <View><Text>Shaheen</Text></View>
+      <View accessibilityLabel={"Shaheen"}
+        testID={"Shaheen"}
+        description={"Shaheen"}>
+        <Text title={"Shaheen"} testID={"Shaheen"}>Shaheen4</Text></View>
     );
+  }
+
+  accessibilityLabel() {
+    return "Shaheen";
+  }
+
+  title() {
+    return "Shaheen";
+  }
+
+  testID() {
+    return "Shaheen";
   }
 }
 
