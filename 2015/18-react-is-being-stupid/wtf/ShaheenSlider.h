0769a48df9edcdba3eef707588d05d18581ecaeb
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-07-22 13:36:28 -0700

    Its compiling - at leasrt for now

---
+++ 2015/18-react-is-being-stupid/wtf/ShaheenSlider.h
@@ -0,0 +1,13 @@
+//
+//  ShaheenSlider.h
+//  wtf
+//
+//  Created by Shaheen Ghiassy on 7/22/15.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import "RCTViewManager.h"
+
+@interface ShaheenSlider : RCTViewManager
+
+@end
