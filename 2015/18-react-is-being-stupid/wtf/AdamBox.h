e30d591da01e073b04858a9019cef18974eede50
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-07-22 14:49:57 -0700

    Its rendering

---
+++ 2015/18-react-is-being-stupid/wtf/AdamBox.h
@@ -0,0 +1,13 @@
+//
+//  AdamBox.h
+//  wtf
+//
+//  Created by Shaheen Ghiassy on 7/22/15.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import "RCTViewManager.h"
+
+@interface AdamBox : RCTViewManager
+
+@end
--- 2015/18-react-is-being-stupid/wtf/AdamBox.js
+++ 2015/18-react-is-being-stupid/wtf/AdamBox.js
@@ -1,7 +1,16 @@
 'use strict';
 
-var React = require('react-native');
-var {Text, View} = React;
+// var React = require('react-native');
+// var {Text, View} = React;
+var NativeMethodsMixin = require('NativeMethodsMixin');
+var PropTypes = require('ReactPropTypes');
+var React = require('React');
+var Text = require('Text');
+// var ReactIOSViewAttributes = require('ReactIOSViewAttributes');
+var StyleSheet = require('StyleSheet');
+var View = require('View');
+// var createReactIOSNativeComponentClass = require('createReactIOSNativeComponentClass');
+var merge = require('merge');
 
 var AdamBox = React.createClass({
   render: function() {
@@ -10,5 +19,19 @@ var AdamBox = React.createClass({
     );
   }
 });
+
+// var validAttributes = {
+//   ...ReactIOSViewAttributes.UIView,
+//   // value: true,
+//   // minimumValue: true,
+//   // maximumValue: true,
+//   // minimumTrackTintColor: true,
+//   // maximumTrackTintColor: true,
+// };
+//
+// var ShaheenSlider = createReactIOSNativeComponentClass({
+//   validAttributes: validAttributes,
+//   uiViewClassName: 'AdamBox',
+// });
 
 module.exports = AdamBox;
