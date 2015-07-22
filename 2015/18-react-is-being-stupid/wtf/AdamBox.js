e1dd45f5d0bbc11bb88315dc2f11f30cb9b95eb1
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-07-22 13:32:00 -0700

    Adbox is rendering

---
+++ 2015/18-react-is-being-stupid/wtf/AdamBox.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var React = require('react-native');
+var {Text, View} = React;
+
+var AdamBox = React.createClass({
+  render: function() {
+    return (
+      <Text>Shaheen</Text>
+    );
+  }
+});
+
+module.exports = AdamBox;
