8a5ae1bbd9efaa036d8f97558f86e4ebb9be2ca2
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-07-24 14:17:15 -0700

    WOrking

--- 2015/18-react-is-being-stupid/wtf/RaptorEngine/PersonView.js
+++ 2015/18-react-is-being-stupid/wtf/RaptorEngine/PersonView.js
@@ -1,12 +1,21 @@
 var React = require('react-native');
-var { Text, View } = React;
+var { StyleSheet, Text, View } = React;
 
 var PersonView = React.createClass({
   render: function() {
     return (
-      <View><Text>{this.props.model.person}</Text></View>
+      <View style={styles.card}><Text>{this.props.model.person}</Text></View>
     );
   },
+});
+
+var styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    backgroundColor: '#CCC',
+    height: 400,
+    borderWidth: 1,
+  }
 });
 
 module.exports = PersonView;
