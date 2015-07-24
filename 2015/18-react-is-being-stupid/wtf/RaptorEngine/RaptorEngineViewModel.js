1cd11c044c8c0cbfb574b3a1f2fcabc1fc00e5cd
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-07-24 11:47:42 -0700

    Interacting with ViewModel support

---
+++ 2015/18-react-is-being-stupid/wtf/RaptorEngine/RaptorEngineViewModel.js
@@ -0,0 +1,12 @@
+var React = require('react-native');
+var { Text, View } = React;
+
+var RaptorEngineViewModel = React.createClass({
+  render: function() {
+    render(
+      <Text>ViewModel</Text>
+    );
+  }
+});
+
+module.exports = RaptorEngineViewModel;
