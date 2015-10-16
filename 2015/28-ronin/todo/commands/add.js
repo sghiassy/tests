e4960f547f27cd605f44b5588a29a1fbe4cbc3c9
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-10-16 01:45:55 -0700

    Added some logic to add

--- 2015/28-ronin/todo/commands/add.js
+++ 2015/28-ronin/todo/commands/add.js
@@ -1,11 +1,26 @@
+var exec = require('child_process').exec,
+  child;
 var Command = require('ronin').Command;
+var choose = require('asking').choose;
+var ask = require('asking').ask;
 
 var Add = Command.extend({
-    desc: 'Add a new task',
+  desc: 'Add a new task',
 
-    run: function () {
+  run: function() {
+    ask('What is your favourite color?', function(err, color) {
+      // color variable contains the answer
+      console.log('color selected', color);
 
-    }
+      child = exec('ls -la', function(err, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (err !== null) {
+          console.log('exec error: ' + error);
+        }
+      });
+    });
+  }
 });
 
 module.exports = Add;
