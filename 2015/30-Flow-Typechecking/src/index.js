0fac0118dca1c0205ef88caf72ba6965d71a915a
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-10-22 01:37:50 -0700

    got more tests running

--- 2015/30-Flow-Typechecking/src/index.js
+++ 2015/30-Flow-Typechecking/src/index.js
@@ -1,10 +1,25 @@
 /* @flow */
 
-// Changing the return type to number fixes the error
-function foo(x: string, y: number): number {
-  var result = x.length * y;
-  console.log(result);
+function length(x) {
+  if (x !== null) {
+    return x.length;
+  } else {
+    return 0;
+  }
+}
+
+var stuff = length('Hello') + length(null);
+console.log(stuff);
+
+
+function total(numbers: Array<number>) {
+  var result = 0;
+
+  for (var i = 0; i< numbers.length; i++) {
+    result += numbers[i];
+  }
+
   return result;
 }
 
-foo('Hello', 42);
+console.log(total([1,2,3,4]));
