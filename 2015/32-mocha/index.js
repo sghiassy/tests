8e6ee23fa23bbb37c6c2d276a96cb90d3ea0b6ea
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-10-27 14:20:15 -0700

    Nested

--- 2015/32-mocha/index.js
+++ 2015/32-mocha/index.js
@@ -1,7 +1,17 @@
 var should = require('should');
 
 describe("I am the first behavior", function() {
+
   it("should work", function() {
     (2).should.equal(2);
   });
+
+  describe("I am the nested describe statement", function() {
+
+    it("I am the nested it statement", function() {
+      "test".should.equal("test");
+    });
+
+  });
+
 });
