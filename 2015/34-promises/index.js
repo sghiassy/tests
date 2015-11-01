b62ca8da57e391016646f68972b324f1f7a88e90
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-10-31 22:08:08 -0700

    Moar

--- 2015/34-promises/index.js
+++ 2015/34-promises/index.js
@@ -1,18 +1,24 @@
 'use strict';
 
 function testPromise() {
-    console.log('Started: Sync code started');
+    console.log('Start of script');
 
     var p1 = new Promise(function(resolve, reject) {
-      console.log('Promise started: Async code started');
+      console.log('Promise started');
 
       setTimeout(function() {
-        resolve('you stupid asshole');
-      }, Math.random() * 2000 + 1000);
+        resolve('the settimeout function completed');
+      }, 1000);
     });
 
     p1.then(function(val) {
       console.log('Promise fulfilled with', val);
+      return "p1.then is returning"
+    }).then(function(val) {
+      console.log('chained promise1', val);
+       return "p1.then is returning for the second chain"
+    }).then(function(val) {
+      console.log('chained promise2', val);
     }).catch(function(reason) {
       console.log('Handle rejected promise ('+reason+') here.');
     });
