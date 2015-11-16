0594680ff3d3d66f981983fddbb10506a302bd82
Shaheen Ghiassy <s.ghiassy@gmail.com>
2015-11-16 15:09:36 -0800

    Initial commit of 36

---
+++ 2015/36-isolated-frameworks/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Shaheen Ghiassy on 11/16/15.
+//  Copyright © 2015 Groupon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 2015/36-isolated-frameworks/test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by Shaheen Ghiassy on 11/16/15.
+//  Copyright © 2015 Groupon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
